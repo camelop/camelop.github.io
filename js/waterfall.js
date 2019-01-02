41be2445a4a5239027bfdbb472c8eb6a05e2ddd5
littleRound <lxy9843@sjtu.edu.cn>
2019-01-02 21:58:08 +0800

    almost finish init

--- js/waterfall.js
+++ js/waterfall.js
@@ -75,7 +75,7 @@ function contentInit(content) {
   // var htmlStr = htmlArr.join('')
   var htmlStr = ''
   for (var i = 0; i < content.length; i++) {
-    htmlStr += '<div class="grid-item">' + '<a class="a-img" href="' + content[i].demo_link + '">' + '<img src="' + content[i].img_link + '">' + ' </a>' + '   <h3 class="demo-title">' + '       <a href="' + content[i].demo_link + '">' + content[i].title + '</a>' + '   </h3>' + '   <p><br>主要技术：</br>' + content[i].core_tech + '</p>' + '   <p>        <a href="' + content[i].code_link + '">源代码 <i class="fa fa-code" aria-hidden="true"></i></a>' + '<br>'+content[i].description+'   </p>' + '</div>'
+    htmlStr += '<div class="grid-item">' + '<a class="a-img" href="' + content[i].demo_link + '">' + '<img src="' + content[i].img_link + '">' + ' </a>' + '   <h3 class="demo-title">' + '       <a href="' + content[i].demo_link + '">' + content[i].title + '</a>' + '   </h3>' + '   <p><strong>主要技术：</strong>' + content[i].core_tech + '</p>' + '   <p>        <a href="' + content[i].code_link + '">源代码 <i class="fa fa-code" aria-hidden="true"></i></a>' + '<br>'+content[i].description+'   </p>' + '</div>'
   }
   var grid = document.querySelector('.grid')
   grid.insertAdjacentHTML('afterbegin', htmlStr)
